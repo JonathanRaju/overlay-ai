57f672ccc4b289cfcb9eeb2e2e5cb0d5c23abd79
Jonathan <jonathan@Jonathans-MacBook-Air.local>
2025-09-16 19:55:18 +0530

    fixed build issue

--- firebase.js
+++ firebase.js
@@ -10,9 +10,9 @@ dotenv.config();
 // const serviceAccount = JSON.parse(
 //   fs.readFileSync("./serviceAccountKey.json", "utf8")
 // );
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-
+const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+  );
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://interview-screening-ba646-default-rtdb.firebaseio.com"
